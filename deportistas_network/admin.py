94586949e40a2404a4148de96397019128b30c9f
Sebiant <sebastianvaneos@gmail.com>
2025-11-19 17:16:26 -0500

    limpieza del archivos basura en el proyecto

--- deportistas_network/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.admin import AdminSite
-
-# Personalizar el admin site
-admin.site.site_header = "Red Social Deportistas - Administración"
-admin.site.site_title = "Panel de Administración"
-admin.site.index_title = "Bienvenido al panel de administración"
-
--- seguimientos/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class SeguimientosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seguimientos'
-    verbose_name = 'Seguimientos y Amistades'
-
